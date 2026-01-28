d39f20a4b10987f4c1e9736ac7b6f30ca464c30a
Illusionqueen <98655406+Illusionqueen@users.noreply.github.com>
2026-01-28 13:07:50 -0500

    Implement summary ranges using array list

---
+++ src/LeetCode/TopInterviewQuestions/Intervals/SummaryRanges.java
@@ -0,0 +1,40 @@
+package LeetCode.TopInterviewQuestions.Intervals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SummaryRanges {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>(); //initializing list to store the resulting ranges
+
+        if (nums == null || nums.length == 0) { //handling edge cases
+            return result;
+        }
+
+        int start = nums[0];
+        for (int i=1; i<=nums.length; i++) {   //looping through an array, going an extra length to close the last range
+            if (i == nums.length || nums[i] != nums[i-1]+1) { //checking when the end of an array is reached or when the numbers are not consecutive
+                int end = nums[i-1]; //end of the range is the previous number
+
+                if (start == end) { //when the range has only one number
+                    result.add(String.valueOf(start));
+                } else {
+                    result.add(start + "->" + end); // if the range has multiple numbers
+                }
+                if (i < nums.length) { // if the end has not reached
+                    start = nums[i]; //start of the next range
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        //int[] nums = {0,1,2,4,5,7};
+        int[] nums = {0,1,2,3,4,6,8,9};
+
+        SummaryRanges sr = new SummaryRanges();
+        List<String> result = sr.summaryRanges(nums);
+        System.out.println(result);
+    }
+}
